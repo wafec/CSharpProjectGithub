f91ad2537a2d2384969dc508510808f79505e3ea
Cardoso <Wallace.Cardoso@philips.com>
2021-05-31 11:09:29 -0300

    first commit

---
+++ WhoAmI.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpProjectGithub
+{
+    using System;
+
+    public class WhoAmI
+    {
+        public string WhoYouThinkYouAre(string name)
+        {
+            return string.Format("Hello {0}", name);
+        }
+    }
+}
